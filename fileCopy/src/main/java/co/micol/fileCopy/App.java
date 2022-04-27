64cefeaab62db0b4b2c8f4847dfdfbe67c1c1e14
admin <admin@YD2-21>
2022-04-27 14:16:11 +0900

    20220427

--- fileCopy/src/main/java/co/micol/fileCopy/App.java
+++ fileCopy/src/main/java/co/micol/fileCopy/App.java
@@ -4,7 +4,7 @@ import co.micol.fileCopy.test.CopyReadWrite;
 
 public class App 
 {
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
     	CopyReadWrite cf = new CopyReadWrite();
         cf.run();
