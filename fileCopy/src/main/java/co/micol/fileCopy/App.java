44ee4beee9e561e4496f496992ea1062485f2c06
admin <admin@YD2-21>
2022-04-27 13:40:05 +0900

    20220427

---
+++ fileCopy/src/main/java/co/micol/fileCopy/App.java
@@ -0,0 +1,17 @@
+package co.micol.fileCopy;
+
+import co.micol.fileCopy.test.CopyRead;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        CopyRead cf = new CopyRead();
+        cf.run();
+        
+    }
+}
