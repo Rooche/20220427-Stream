18b913964d3fbe93765d47d7fc40c4e13540b8b7
admin <admin@YD2-21>
2022-04-27 10:23:17 +0900

    20220427

---
+++ streamTest/src/main/java/co/micol/streamTest/App.java
@@ -0,0 +1,13 @@
+package co.micol.streamTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
